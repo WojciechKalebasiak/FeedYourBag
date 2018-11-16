8a2b697fd1e11f951ff3284549c4e9b3572ca861
Wojciech Kałębasiak <kalebasiakw@gmail.com>
2018-11-16 23:08:37 +0100

    Create OAuth login

--- server/index.js
+++ server/index.js
@@ -1,27 +1,18 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
 const PORT = process.env.PORT || 5000;
 const app = express();
+const keys = require("./config/keys");
+const mongoose = require("mongoose");
+//Connect to mongo
+mongoose.connect(keys.mongoURI);
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken, refreshToken, profile);
-    }
-  )
-);
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-app.get("/auth/google/callback", passport.authenticate('google'));
+//Pasport Config
+require("./services/passport");
+
+//Auth router
+const auth = require("./routes/authRoutes");
+
+app.use("/auth", auth);
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
