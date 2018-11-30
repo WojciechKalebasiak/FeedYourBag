b5435f0095f93f8821bf0038ab28b8e2535a4f2e
Wojciech Kałębasiak <kalebasiakw@gmail.com>
2018-11-30 03:40:00 +0100

    Add not found for unrouted paths

---
+++ client/src/components/NotFound/NotFound.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <h1 style={{ margin: "0 auto", marginTop: "15%", textAlign:'center' }}>
+      We couldn't find what you were looking for.
+    </h1>
+  );
+}
--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { fetchUser } from "../../actions";
 import { connect } from "react-redux";
 import Dashboard from "../Dashboard/Dashboard";
 import Header from "../Header/Header";
 import Home from "../../components/Home/Home";
 import SurveyNew from "../surveys/SurveyNew";
+import NotFound from "../../components/NotFound/NotFound";
 class App extends Component {
   componentDidMount() {
     this.props.fetchUser();
@@ -16,9 +17,12 @@ class App extends Component {
         <React.Fragment>
           <Header />
           <div className="container">
-            <Route path="/" exact component={Home} />
-            <Route path="/dashboard" exact component={Dashboard} />
-            <Route path="/new-survey" component={SurveyNew} />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/dashboard" exact component={Dashboard} />
+              <Route path="/new-survey" component={SurveyNew} />
+              <Route path="*" component={NotFound} />
+            </Switch>
           </div>
         </React.Fragment>
       </BrowserRouter>
