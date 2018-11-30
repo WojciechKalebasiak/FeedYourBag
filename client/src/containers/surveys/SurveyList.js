599ae57ad92694fc6d0e26fbd45100f906c36456
Wojciech Kałębasiak <kalebasiakw@gmail.com>
2018-11-30 16:04:40 +0100

    Fix Survey List key prop and credits requirments

--- client/src/containers/surveys/SurveyList.js
+++ client/src/containers/surveys/SurveyList.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchSurveys } from "../../actions/index";
 import SurveyListItem from "./SurveyListItem";
-import styles from './SurveyList.module.css';
+import styles from "./SurveyList.module.css";
 class SurveyList extends Component {
   componentDidMount() {
     this.props.getSurveys();
@@ -12,7 +12,7 @@ class SurveyList extends Component {
     return (
       <ul className={styles.SurveyList}>
         {surveys.reverse().map(survey => (
-          <SurveyListItem survey={survey} />
+          <SurveyListItem key={survey._id} survey={survey} />
         ))}
       </ul>
     );
--- server/middleware/requireCredits.js
+++ server/middleware/requireCredits.js
@@ -2,7 +2,7 @@ const requireCredits = (req, res, next) => {
   const { recipients } = req.body;
   const amountOfMails = recipients
     .split(",")
-    .map(email => ({ email: email.trim() })).lenght;
+    .map(email => ({ email: email.trim() })).length;
   if (req.user.credits - amountOfMails < 0) {
     return res.status(402).send({ error: "Not enough credits!" });
   }
